5171de54525950c5348ed729993cd10fba2bb98e
NALLAPANENILAKSHMISOWJANYA <2300031099@kluniversity.in>
2025-09-03 20:58:40 +0530

    update pom.xml

--- SPRINGBOOT-ECOMMERCEAPI/src/main/java/com/klef/jenkins/SpringbootEcommerceapiApplication.java
+++ SPRINGBOOT-ECOMMERCEAPI/src/main/java/com/klef/jenkins/SpringbootEcommerceapiApplication.java
@@ -2,9 +2,10 @@ package com.klef.jenkins;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class SpringbootEcommerceapiApplication {
+public class SpringbootEcommerceapiApplication extends SpringBootServletInitializer{
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringbootEcommerceapiApplication.class, args);
